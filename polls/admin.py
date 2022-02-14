0dc1725a5b61914a47455a99d3fe79a2b703502e
lubshad <lubshad4u4@gmail.com>
2022-02-14 12:08:07 +0530

    form and admin customization done

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,5 @@
+from dataclasses import fields
+from random import choices
 from django.contrib import admin
 
 from polls.models import Choice, Question
@@ -5,5 +7,25 @@ from polls.models import Choice, Question
 # Register your models here.
 
 
-admin.site.register(Question)
+class ChoiceInlines(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Question Information", {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["published_date"]}),
+    ]
+
+    inlines = [ChoiceInlines]
+    
+    list_display = ("question_text", "published_date", "was_published_recently")
+
+    list_filter = ["published_date"]
+
+    search_fields = ['question_text']
+    list_per_page  = 2
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,8 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+from django.contrib import admin
+
 
 # Create your models here.
 
@@ -7,8 +11,18 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     published_date = models.DateTimeField("date published")
 
+    
+
     def __str__(self) -> str:
         return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    def was_published_recently(self) -> bool:
+        return timezone.now() - datetime.timedelta(days=1) <= self.published_date
 
 
 class Choice(models.Model):
